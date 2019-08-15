af3c1c8880ebd3fc795ac6a130e044402343faef
Shadman Rohan <39516236+ShadmanRohan@users.noreply.github.com>
2019-08-16 00:21:51 +0600

    Update loss.py

--- src/loss.py
+++ src/loss.py
@@ -8,19 +8,25 @@ import torch.nn as nn
 from utils.vocabulary import Vocabulary
 from src.models import Discriminator
 
-
+######################################################################################################################################
+# Object to calculate Discriminator Loss
+######################################################################################################################################
 class DiscriminatorLossCompute:
     def __init__(self, discriminator: Discriminator):
         self.discriminator = discriminator
         self.criterion = nn.BCELoss(size_average=False)
-
+######################################################################################################################################
+# Pass the Encoder-Output, Target to this Function........ It will return the adversarial loss (Binary Cross Entropy)
+######################################################################################################################################
     def compute(self, encoder_output, target):
         log_prob = self.discriminator.forward(encoder_output)
         log_prob = log_prob.view(-1)
         adv_loss = self.criterion(log_prob, target)
         return adv_loss
 
-
+######################################################################################################################################
+# Sum of Negative Loglikelihood Loss Between SCORES and TARGET is returned
+######################################################################################################################################
 class MainLossCompute:
     def __init__(self, vocabulary: Vocabulary, use_cuda):
         weight = torch.ones(vocabulary.size())
@@ -31,6 +37,7 @@ class MainLossCompute:
 
     def compute(self, scores, target):
         loss = 0
+############# I do not understand size()   ###############     
         for t in range(target.size(0)):
             loss += self.criterion(scores[t], target[t])
         return loss
